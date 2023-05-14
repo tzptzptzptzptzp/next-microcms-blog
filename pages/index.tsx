620b75e5ab0ca22a677841dfff7c628c01a7f8e7
tzptzptzptzptzp <tzp2539@gmail.com>
2023-05-14 14:51:28 +0900

    ✨ microCMSから投稿データを取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
+import { client } from '../libs/client'
+import type { Blog } from '../src/type/blog'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+// SSG
+export const getStaticProps = async () => {
+  const blog = await client.get({ endpoint: 'blog' })
+  return {
+    props: {
+      blogs: blog.contents,
+    }
+  }
+}
+
+type Props = {
+  blogs: Blog[]
+}
+
+export default function Home({ blogs }: Props) {
   return (
     <div className='wrapper'>
-
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <p>{blog.title}</p>
+          <div dangerouslySetInnerHTML={{ __html: `${blog.body}` }}></div>
+          <img src={blog.thumbnail.url} alt={blog.title} />
+        </div>
+      ))}
     </div>
   )
 }
