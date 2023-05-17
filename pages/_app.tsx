ea848863e4f26b64c680e637578abdb1eea81a53
tzptzptzptzptzp <tzp2539@gmail.com>
2023-05-18 01:41:32 +0900

    ✨ GoogleFontsを読み込み

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
