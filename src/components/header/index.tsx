ca54f7c1451604a02c15851d89bdb68d777b979d
tzptzptzptzptzp <tzp2539@gmail.com>
2023-05-19 01:45:16 +0900

    ✨ ヘッダーの高さを管理する処理を追加

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,18 @@
+import React from 'react'
 import { Logo } from './Logo'
 import { PCNavigation } from '../navigation/PC'
 import { SPNavigation } from '../navigation/SP'
 
 import useDeviceType from '../../hooks/useDeviceType'
+import useElementHeight from '../../hooks/useElementHeight'
 
 export function Header() {
   const { isDesktop } = useDeviceType();
+
+  const [elementRef] = useElementHeight();
+
   return (
-    <header className="fixed  w-full p-12 s:p-6 bg-bg">
+    <header ref={elementRef} className="fixed  w-full p-12 s:p-6 bg-bg">
       <div className='flex items-center justify-between max-w-[1920px] m-auto'>
         <Logo></Logo>
         {isDesktop ? <PCNavigation></PCNavigation> : <SPNavigation></SPNavigation>}
