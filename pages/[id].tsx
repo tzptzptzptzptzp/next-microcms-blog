6a84d95a59b79ab2ddcf65f70ec20be3a0589454
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-06 16:53:27 +0900

    ✨ ブログ詳細ページを作成

---
+++ pages/[id].tsx
@@ -0,0 +1,17 @@
+import type { Blog } from '../src/type/blog'
+
+import { Header } from '../src/components/header'
+import { Footer } from '../src/components/footer'
+
+export default function Blog() {
+  return (
+    <div className='wrapper bg-bg text-text'>
+      <Header></Header>
+      <div>
+        <div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  )
+}
