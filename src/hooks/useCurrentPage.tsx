22abf0e73bdc6b9624cd5e8cf4d154aa0c63a55d
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-09 02:15:16 +0900

    🐛 初回時に値を取得できない不具合を修正

--- src/hooks/useCurrentPage.tsx
+++ src/hooks/useCurrentPage.tsx
@@ -1,24 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 const useCurrentPage = () => {
-  const [currentPage, setCurrentPage] = useState('');
+  const [currentPage, setCurrentPage] = useState('')
 
   useEffect(() => {
     const handlePageChange = () => {
-      const path = location.pathname; // 現在のパスを取得
-      setCurrentPage(path);
+      const path = location.pathname // 現在のパスを取得
+      setCurrentPage(path)
     };
 
+    // 初期表示時にパスを取得
+    handlePageChange()
+
     // ページ変更のイベントリスナーを追加
-    addEventListener('popstate', handlePageChange);
+    addEventListener('popstate', handlePageChange)
 
     // コンポーネントのアンマウント時にイベントリスナーをクリーンアップ
     return () => {
-      removeEventListener('popstate', handlePageChange);
-    };
-  }, []);
+      removeEventListener('popstate', handlePageChange)
+    }
+  }, [])
 
-  return currentPage;
-};
+  return currentPage.trim() !== '' ? currentPage : null
+}
 
-export default useCurrentPage;
+export default useCurrentPage
