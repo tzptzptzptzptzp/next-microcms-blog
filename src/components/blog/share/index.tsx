1a1a09a634a95b08497c1891b882ef4331969836
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-12 18:39:28 +0900

    ✨ シェア用リンクを作成

---
+++ src/components/blog/share/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import clipboardCopy from 'clipboard-copy'
+import { Blog } from "../../../type/blog"
+
+import useDeviceType from "../../../hooks/useDeviceType"
+
+type Blogs = {
+  blog: Blog
+}
+
+export function Share({ blog }: Blogs) {
+  const { isMobile } = useDeviceType()
+  const url = useRouter().asPath
+
+  const handleClick = (url: string) => {
+    clipboardCopy(url)
+      .then(() => {
+        alert(`コピーしました！\n${url}`)
+      })
+      .catch((error: Error) => {
+        alert('コピーに失敗しました。')
+      })
+  }
+
+  const handleCopy = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    handleClick(`https://あれこれ.website${url}/`)
+  }
+
+  const secTtl = (category: string) => {
+    if (category === 'recipe') {
+      return 'レシピ'
+    } else if (category === 'javascript') {
+      return 'JavaScript'
+    } else if (category === 'css') {
+      return 'CSS'
+    } else if (category === 'html') {
+      return 'HTML'
+    }
+  }
+
+  return (
+    <div className="mb-32 s:mb-16 px-8 s:px-6">
+      <div className="flex gap-8 justify-center m-auto text-bg text-[1.8rem]leading-tight">
+        <a className='flex items-center justify-center gap-1 w-[17.5rem] s:w-14 h-auto s:h-14 py-3 rounded-lg s:rounded-full bg-[#73C7FB]' href={`https://twiter.com/share?url=https://あれこれ.website/${url}&text=【${secTtl(blog.category[0])}】${blog.title}｜WEBのあれこれ`} rel="nofollow noopener" target="_blank">
+          <Image src='/img/links/share_twitter.svg' className='w-auto h-[18px]' style={{ transform: 'translateY(1px)' }} width={22} height={18} alt={blog.title} />
+          {isMobile ? null : 'ついったー'}
+        </a>
+        <a className='flex items-center justify-center gap-1 w-[17.5rem] s:w-14 h-auto s:h-14 py-3 rounded-lg s:rounded-full bg-[#768FC1]' href={`http://www.facebook.com/share.php?u=https://あれこれ.website/${url}`} rel="nofollow noopener" target="_blank">
+          <Image src='/img/links/share_facebook.svg' className='w-auto h-[18px]' style={{ transform: 'translateY(1px)' }} width={22} height={18} alt={blog.title} />
+          {isMobile ? null : 'ふぇいすぶっく'}
+        </a>
+        <button className='flex items-center justify-center gap-1 w-[17.5rem] s:w-14 h-auto s:h-14 py-3 rounded-lg s:rounded-full bg-[#BBBBBB]' onClick={handleCopy}>
+          <Image src='/img/links/share_link.svg' className='w-auto h-[18px]' style={{ transform: 'translateY(1px)' }} width={22} height={18} alt={blog.title} />
+          {isMobile ? null : 'りんくこぴー'}
+        </button>
+      </div>
+    </div>
+  )
+}
