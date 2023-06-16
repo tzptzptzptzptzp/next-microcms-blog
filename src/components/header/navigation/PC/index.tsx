cda57865554a2d35bcec3d7f2585b9a4d9a494ff
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-17 02:43:33 +0900

    🐛 下層ページでリンクが機能しない不具合を修正

--- src/components/header/navigation/PC/index.tsx
+++ src/components/header/navigation/PC/index.tsx
@@ -3,19 +3,19 @@ export function PCNavigation() {
     <nav className="h-full block">
       <ul className="flex gap-16 h-full text-[2.5rem]">
         <li>
-          <a href="#" className="h-circle">トップ</a>
+          <a href="/#" className="h-circle">トップ</a>
         </li>
         <li>
-          <a href="#recipe" className="h-circle">レシピ</a>
+          <a href="/#recipe" className="h-circle">レシピ</a>
         </li>
         <li>
-          <a href="#js" className="h-circle">JavaScript</a>
+          <a href="/#javascript" className="h-circle">JavaScript</a>
         </li>
         <li>
-          <a href="#css" className="h-circle">CSS</a>
+          <a href="/#css" className="h-circle">CSS</a>
         </li>
         <li>
-          <a href="#html" className="h-circle">HTML</a>
+          <a href="/#html" className="h-circle">HTML</a>
         </li>
       </ul>
     </nav>
