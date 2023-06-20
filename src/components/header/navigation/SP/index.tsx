3c6b000960d52ed4065331e6d5ae18659803cad4
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-20 22:06:16 +0900

    ✨ メニュー開閉の処理を追加

--- src/components/header/navigation/SP/index.tsx
+++ src/components/header/navigation/SP/index.tsx
@@ -1,18 +1,31 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+
+import { menuState } from '../../../../states/menuState'
+
 import useCurrentPage from "../../../../hooks/useCurrentPage"
 
 export function SPNavigation() {
-  const currentPage = useCurrentPage();
+  const currentPage = useCurrentPage()
 
-  let content;
+  const [menuOpen, setMenuOpen] = useRecoilState(menuState)
+
+  // メニューボタンの状態を更新
+  const handleClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
+    setMenuOpen(!menuOpen)
+  }, [menuOpen, setMenuOpen])
+
+  // ページに毎に表示するコンテンツを管理
+  let content
   if (currentPage === '' || currentPage === '/') {
     content = <ul className="flex gap-8">
-      <li>
+      <li className='cursor-pointer'>
         <svg width="34" height="20" viewBox="0 0 34 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M31 17L17 3" stroke="#301E1E" stroke-width="5.83333" stroke-linecap="round" />
           <path d="M3 17L17 3.00001" stroke="#301E1E" stroke-width="5.83333" stroke-linecap="round" />
         </svg>
       </li>
-      <li>
+      <li className='cursor-pointer'>
         <svg width="34" height="20" viewBox="0 0 34 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M3 3L17 17" stroke="#301E1E" stroke-width="5.83333" stroke-linecap="round" />
           <path d="M31 3L17 17" stroke="#301E1E" stroke-width="5.83333" stroke-linecap="round" />
@@ -20,10 +33,10 @@ export function SPNavigation() {
       </li>
     </ul>
   } else {
-    content = <div className="relative w-12 h-8">
-      <span className="absolute top-0 w-full h-[4px] rounded-full bg-text"></span>
-      <span className="absolute centering-y w-full h-[4px] rounded-full bg-text"></span>
-      <span className="absolute bottom-0 w-full h-[4px] rounded-full bg-text"></span>
+    content = <div onClick={handleClick} className="relative w-12 h-8">
+      <span className={`absolute w-full h-[4px] rounded-full bg-text`} style={{ top: menuOpen ? '50%' : '0', transform: menuOpen ? 'translateY(-50%) rotate(45deg)' : '', transition: '.3s' }}></span>
+      <span className={`absolute centering-y w-full h-[4px] rounded-full bg-text`} style={{ top: menuOpen ? '50%' : '50%', transform: menuOpen ? 'translateY(-50%) rotate(135deg)' : '', transition: '.3s' }}></span>
+      <span className={`absolute bottom-0 w-full h-[4px] rounded-full bg-text`} style={{ bottom: menuOpen ? '50%' : '0', transform: menuOpen ? 'translateY(50%) rotate(-135deg)' : '', transition: '.3s' }}></span>
     </div>
   }
 
