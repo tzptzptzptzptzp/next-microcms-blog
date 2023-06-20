95b27a2743b263f9fc6e971d9cf2f9e6011adc12
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-21 02:37:53 +0900

    ✨ ファビコンを設定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
+    const faviconPath = 'favicon'
     return (
       <Html lang='ja'>
         <Head>
+          <link rel="icon" type="image/png" sizes="16x16" href={`${faviconPath}/favicon-16x16.png`} />
+          <link rel="icon" type="image/png" sizes="32x32" href={`${faviconPath}/favicon-32x32.png`} />
+          <link rel="icon" type="image/png" sizes="192x192" href={`${faviconPath}/android-icon-192x192.png`} />
+          <link rel="apple-touch-icon" sizes="180x180" href={`${faviconPath}/apple-icon-180x180.png`} />
+          <link rel="manifest" href={`${faviconPath}/manifest.json`} />
+
+          <meta name="msapplication-TileImage" content={`${faviconPath}/ms-icon-144x144.png`} />
+          <meta name="msapplication-TileColor" content="#fed54d" />
+          <meta name="theme-color" content="#fed54d" />
         </Head>
         <body>
           <Main />
