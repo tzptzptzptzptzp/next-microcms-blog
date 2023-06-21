a21fd8c9f4bf35c60d70cefd7161226c8384e809
tzptzptzptzptzp <tzp2539@gmail.com>
2023-06-22 02:59:03 +0900

    ✨ 各ページのHead情報を作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import NextHeadSeo from 'next-head-seo'
 import { client } from '../libs/client'
 
 import { Header } from '../src/components/header'
@@ -23,14 +24,29 @@ type Blogs = {
 
 export default function Home({ blogs }: Blogs) {
   return (
-    <div className='wrapper bg-bg text-text'>
-      <Header position='fixed' bg='bg-bg'></Header>
-      <KeyVisual></KeyVisual>
-      <BlogLists category='recipe' blogs={blogs}></BlogLists>
-      <BlogLists category='javascript' blogs={blogs}></BlogLists>
-      <BlogLists category='css' blogs={blogs}></BlogLists>
-      <BlogLists category='html' blogs={blogs}></BlogLists>
-      <Footer></Footer>
-    </div>
+    <>
+      <NextHeadSeo
+        title='WEBのあれこれ | WEBのGOODな情報を発信中！'
+        description='コピペで使えるJavaScriptレシピ(TypeScript対応)やよりサイト制作を行いやすくなるノウハウを発信中'
+        canonical='https://xn--l8j8a4kb.website/'
+        og={{
+          title: 'WEBのあれこれ | WEBのGOODな情報を発信中！',
+          description: 'コピペで使えるJavaScriptレシピ(TypeScript対応)やよりサイト制作を行いやすくなるノウハウを発信中',
+          image: 'https://xn--l8j8a4kb.website/img/ogp.jpg',
+          type: 'website',
+          siteName: 'WEBのあれこれ'
+        }}
+        twitter={{ card: "summary_large_image" }}
+      />
+      <div className='wrapper bg-bg text-text'>
+        <Header position='fixed' bg='bg-bg'></Header>
+        <KeyVisual></KeyVisual>
+        <BlogLists category='recipe' blogs={blogs}></BlogLists>
+        <BlogLists category='javascript' blogs={blogs}></BlogLists>
+        <BlogLists category='css' blogs={blogs}></BlogLists>
+        <BlogLists category='html' blogs={blogs}></BlogLists>
+        <Footer></Footer>
+      </div>
+    </>
   )
 }
