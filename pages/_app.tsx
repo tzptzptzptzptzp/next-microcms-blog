87fabb050e75184b2445d9153cdc13cc50318068
tzptzptzptzptzp <git@tzp.pink>
2024-01-31 01:09:42 +0900

    🐛 エラーを解消

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
-import '../styles/globals.css'
-import Head from 'next/head'
-import { RecoilRoot } from 'recoil'
-import { AnimatePresence } from 'framer-motion'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
+import { AnimatePresence } from "framer-motion";
+import type { AppProps } from "next/app";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <RecoilRoot>
-        <Head>
-        </Head>
-        <AnimatePresence mode='wait'>
-          <Component {...pageProps} />
-        </AnimatePresence>
-      </RecoilRoot>
-    </>
-  )
+    <RecoilRoot>
+      <Head children={undefined}></Head>
+      <AnimatePresence mode="wait">
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </RecoilRoot>
+  );
 }
